e58cbb056afa09f9a9aa79a89e2e7c60bd2a992c
Brad Lotocki <brad.lotocki@gmail.com>
2023-11-03 11:26:05 -0600

    subscribe to pose wip

--- src/turtle_food/include/turtlesim/food.h
+++ src/turtle_food/include/turtlesim/food.h
@@ -5,6 +5,13 @@
 #include <string>
 #include <unistd.h>
 #include <iostream>
+#include <boost/shared_ptr.hpp>
+#include "std_msgs/String.h"
+
+#include <QImage>
+#include <QPainter>
+#include <QPen>
+#include <QPointF>
 
 // TODO namespace wont build, is this needed?
 // namespace turtlesim
@@ -15,11 +22,14 @@ class Food
 	public:
 		Food(int& argc, char** argv);
 		void print_food_info();
+		void positionCallback(const std_msgs::String::ConstPtr& msg);
 
 	private:
 		std::string name_;
 		int calories_;
-
+		QPointF pos_x_;
+		QPointF pos_y_;
+		ros::NodeHandle n_;
 };
 
 #endif // TURTLE_FOOD_H
--- src/turtle_food/src/food.cpp
+++ src/turtle_food/src/food.cpp
@@ -6,7 +6,7 @@
 // class: contains name of class
 Food::Food(int& argc, char** argv) : name_("base food"), calories_(0)
 {
-	ros::init(argc, argv, "food_node");
+	ros::Subscriber sub = n_.subscribe("pose", 1, &Food::positionCallback, this);
 }
 
 // print loop: just prints using ROS_INFO
@@ -15,13 +15,21 @@ void Food::print_food_info()
 	ROS_INFO("Food name = %s, calories = %d", name_.c_str(), calories_);
 }
 
+// callback to lister to turtle position
+void Food::positionCallback(const std_msgs::String::ConstPtr& msg)
+{
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
 // main: create class -> run print loop
 int main(int argc, char** argv)
 {
+	ros::init(argc, argv, "food_node");
 	Food new_food = Food(argc, argv);
 
 	while (true)
 	{
+		ros::spin();
 		new_food.print_food_info();
 		sleep(1);
 	}
