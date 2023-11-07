d13b96664e2021ecebe5393a4bbfa81369336427
Brad Lotocki <brad.lotocki@gmail.com>
2023-11-06 18:12:51 -0700

    script work

--- run_turtle_food.sh
+++ run_turtle_food.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-source /opt/ros/kinetic/setup.bash
+# source
 dir=$PWD
+source /opt/ros/kinetic/setup.bash
 eval "source $dir/devel/setup.bash"
-# echo "$dir"
-# echo "$(printenv | grep ROS)"
 
+# run program
 roscore &
 sleep 2
 rosrun turtle_food turtlesim_node & 
 rosrun turtle_food food_node "$@" &
+bash $dir/run_turtle.sh
 wait
-
-# roslaunch turtle_food sim.launch
